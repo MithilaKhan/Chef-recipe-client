02c33d45d6ee6a0fecc4d3aa307ac4109a1cd22c
MithilaKhan <mithilakhan082@gmail.com>
2023-05-03 12:25:49 +0600

    loaded data form server

---
+++ src/Components/Chef/Chef.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Chef = ({chef}) => {
+   const { chef_picture ,chef_name, years_of_experience ,number_of_recipes ,likes} =chef
+   return (
+      <div>
+         
+      </div>
+   );
+};
+
+export default Chef;
--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, {  useEffect, useState } from 'react';
 import Banner from '../Components/Banner/Banner';
 import Categories from '../Components/Categories/Categories';
 import FoodItems from '../Components/FoodItems';
+import Chef from '../Components/Chef/Chef';
 
 const Home = () => {
+   const [chefs , setChefs] = useState([])
+
+   useEffect(()=>{
+      fetch('https://chef-recipe-hunter-server-side-mithilakhan.vercel.app/chef')
+      .then(res => res.json())
+      .then(data => setChefs(data))
+   } ,[])
    return (
       <div>
          <Banner/>
@@ -18,6 +26,20 @@ const Home = () => {
          <h1 className='text-pink-500 text-4xl  font-bold'>Famous Food Items</h1>
           </div>
           <FoodItems/>
+
+          <div className='text-center pt-16 pb-14'>
+          <p className='italic  text-purple-500 text-xl'>Our Delicious & </p>
+         <h1 className='text-pink-500 text-4xl  font-bold'>Chefs</h1>
+          </div>
+          <div className='grid lg:grid-cols-3 sm:grid-cols-1 gap-6 justify-items-center'>
+            {
+               chefs.map(chef =><Chef
+               key={chef.id}
+               chef={chef}
+               ></Chef>)
+            }
+          </div>
+         
       </div>
    );
 };
